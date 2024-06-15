7fee43876824a53df67fddd954c72db63e51bf97
YecidMorenoUSP <yecidmoreno@usp.br>
2024-06-14 22:10:46 -0300

    mqtt add SD logger

--- src/use_mqtt.cpp
+++ src/use_mqtt.cpp
@@ -1,12 +1,42 @@
 #include "use_mqtt.h"
 
+
 const char* TAG_MQTT = "USE_MQTT";
+
+
+void My_PubSubClient::initQueue() {
+  stringQueue = xQueueCreate(MAX_MQTT_SAVED, sizeof(MQTT_Pair));
+  if (stringQueue == NULL) {
+      ESP_LOGI(TAG_MQTT,"Failed to create the queue.\n");
+  }
+}
+
+void My_PubSubClient::addToMQTTQueue(const char* data1, const char* data2) {
+    MQTT_Pair pair;
+    strncpy(pair.topic, data1, MAX_TOPIC_LENGTH);
+    strncpy(pair.msg, data2, MAX_MSG_LENGTH);
+    if (xQueueSend(stringQueue, &pair, portMAX_DELAY) != pdPASS) {
+      ESP_LOGI(TAG_MQTT,"Failed to send data to the queue.\n");
+    }
+    MQTT_FIFO++;
+}
+
+bool My_PubSubClient::getToMQTTQueue(MQTT_Pair& Buffer) {
+    if (xQueueReceive(stringQueue, &Buffer, portMAX_DELAY) == pdTRUE) {
+      MQTT_FIFO--;
+      return true;
+    } else {
+      ESP_LOGI(TAG_MQTT,"Failed to receive data from the queue.\n");
+      return false;
+    }
+}
 
 My_PubSubClient::My_PubSubClient(Client& client)
   : PubSubClient(client) {
   setReconnectCallback(this->_my_reconnect_callback);
   _setUser("my_user");
   _client = &client;
+  initQueue();
 }
 
 My_PubSubClient::~My_PubSubClient() {
@@ -22,11 +52,15 @@ My_PubSubClient& My_PubSubClient::setReconnectCallback(MQTT_RECONNECT_CALLBACK_S
   return *this;
 }
 
-
 bool My_PubSubClient::_publish(const char* payload, const char* message, bool LOG) {
 
   if (LOG) {
     // Save to SD
+    if (SD_INSERTED()){
+      SD_appendFile(SD, "/MQTT_OUT.log", payload,'\n');
+    }else{
+
+    }
   }
 
   bool res = this->publish(payload, message);
@@ -34,25 +68,76 @@ bool My_PubSubClient::_publish(const char* payload, const char* message, bool LO
   if (!res) {
     if (LOG) {
       // Save idx
-      MQTT_FIFO++;
+      if (SD_INSERTED()){
+        SD_appendFile(SD, "/MQTT_FIFO.log", payload,'\n');
+        SD_appendFile(SD, "/MQTT_FIFO.log", message,'\n');
+      }else{
+        this->addToMQTTQueue(payload,message);
+      }
     }
   }
-
   return res;
 }
 
 void My_PubSubClient::_publish_saved(long idx) {
   if (idx == -1) {
     if (MQTT_FIFO > 0) {
-      // Read SD - idx
-      bool res = this->_publish("bf_test/try_again", ".", false);
-      if (res) {
-        // Remove SD - idx
-        ESP_LOGI(TAG_MQTT, "Resended OK");
-        MQTT_FIFO--;
-      } else {
-        ESP_LOGI(TAG_MQTT, "Resended FAIL");
+      
+      MQTT_Pair rec;
+      if(getToMQTTQueue(rec)){
+
+        if (this->_publish(rec.topic,rec.msg, false)) {
+          ESP_LOGI(TAG_MQTT, "Resended OK");
+        } else {
+          ESP_LOGI(TAG_MQTT, "Resended FAIL");
+          addToMQTTQueue(rec.topic,rec.msg);
+        }
       }
+      
+
+    }else if (SD_INSERTED()){
+
+        if (SD_takeMutex()){
+          
+          File file = SD.open("/MQTT_FIFO.log");
+          if (!file) {
+            ESP_LOGI(SD_TAG, "Failed to open file for reading");
+            SD_releaseMutex();
+            return;
+          }
+
+          if(!file.available()){
+            file.close();
+            SD_releaseMutex();
+            return;
+          }
+
+          char topic_tmp[100];
+          char msg_tmp[100];
+
+          size_t pos_topic = SD_readFileOpennedUntil(file,topic_tmp,0,'\n');
+          size_t pos_msg = SD_readFileOpennedUntil(file,msg_tmp,pos_topic+1,'\n');
+
+          file.close();
+          SD_releaseMutex();
+    
+          
+          ESP_LOGI(TAG_MQTT,"Try to send in");
+          ESP_LOGI(TAG_MQTT,"[%s] [%s]",topic_tmp,msg_tmp);
+
+
+          bool res = this->_publish(topic_tmp, msg_tmp, false);
+
+          if (res) {
+            SD_cropfile(SD,"/MQTT_FIFO.log",pos_topic+pos_msg+2);
+          } else {
+            ESP_LOGI(TAG_MQTT, "Resended FAIL");
+          }
+          
+        }else{
+          return;
+        }
+        
     }
   }
 }
@@ -70,7 +155,7 @@ void My_PubSubClient::_TaskReconnect() {
     if (!this->connected()) {
       ESP_LOGI(TAG_MQTT, "Not Connected");
       long now = millis();
-      if (now - lastReconnectAttempt > 5000) {
+      if (now - lastReconnectAttempt > this->wait_to_reconnect) {
         lastReconnectAttempt = now;
         // Attempt to reconnect
         if (this->_reconnect()) {
@@ -80,13 +165,13 @@ void My_PubSubClient::_TaskReconnect() {
       }
     } else {
       // Client connected
-      for (int i = 0; i < 5; i++)
-        this->_publish_saved(-1);
+      this->_publish_saved(-1);
 
       this->loop();
     }
+    
 
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+    vTaskDelay(this->delay_for_loop / portTICK_PERIOD_MS);
   }
 }
 
--- src/use_mqtt.h
+++ src/use_mqtt.h
@@ -7,11 +7,25 @@
 //  https://github.com/knolleary/pubsubclient
 //  git checkout v2.8
 #include <PubSubClient.h>
+#include "utils_sd.h"
+
+#include <string.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+
+#define MAX_MQTT_SAVED 10
+#define MAX_TOPIC_LENGTH 100
+#define MAX_MSG_LENGTH 100
 
 #if defined(ESP8266) || defined(ESP32)
 #include <functional>
 #define MQTT_RECONNECT_CALLBACK_SIGNATURE std::function<void(My_PubSubClient& obj)> reconnect_callback
 #endif
+
+typedef struct {
+    char topic[MAX_TOPIC_LENGTH];
+    char msg[MAX_MSG_LENGTH];
+} MQTT_Pair;
 
 class My_PubSubClient : public PubSubClient {
 private:
@@ -20,6 +34,13 @@ private:
     long int MQTT_FIFO = 0;
     long lastReconnectAttempt = 0;
     MQTT_RECONNECT_CALLBACK_SIGNATURE;
+    long wait_to_reconnect = 5000;
+    long delay_for_loop = 1000;
+    QueueHandle_t stringQueue;
+
+    void initQueue();
+    void addToMQTTQueue(const char* data1, const char* data2);
+    bool getToMQTTQueue(MQTT_Pair& Buffer);
 
 public:
     My_PubSubClient& setReconnectCallback(MQTT_RECONNECT_CALLBACK_SIGNATURE);
